1ec5717ce3390a6aa0ff4648174f3b095f7b220e
John Leschinski <picard102@gmail.com>
2015-11-04 16:21:55 -0500

    JE Design

---
+++ index.php
@@ -0,0 +1,8 @@
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<meta http-equiv="Content-Style-Type" content="text/css">
+<title></title>
+<body>
+<p class="p1"><img src="img/raster/compressed/stae logo.jpg" alt="stae logo.jpg"></p>
+
+</body></html>
